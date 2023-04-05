58306144a487d36ded473f7f3dfbed42d951ccdc
Luna T <luna_trn@tutanota.com>
2023-04-05 22:26:52 +1000

    Functional Gravity Ability
    Author: Luna

--- src/main/java/me/luna/lunapvp/PlayerInstance.java
+++ src/main/java/me/luna/lunapvp/PlayerInstance.java
@@ -2,9 +2,8 @@ package me.luna.lunapvp;
 
 import java.util.UUID;
 
-import org.bukkit.entity.Player;
-
 import me.luna.playerClasses.AbilityTemplate;
+import org.bukkit.entity.Player;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 
@@ -13,14 +12,14 @@ public class PlayerInstance {
 	public boolean isPlayerDead = false;
 	private String team = "";
 	private long lastActivation = 0;
-	private UUID playerUUID;
-	private int ABILITY_COOLDOWN_SECONDS = 5;
+	private final UUID playerUUID;
+	private final int ABILITY_COOLDOWN_SECONDS = 5;
 	public PlayerInstance(UUID playerUUID){
 		this.playerUUID = playerUUID;
 	}
 
 	private boolean hasCooldownPassed(){
-		return lastActivation - System.currentTimeMillis() > ABILITY_COOLDOWN_SECONDS;
+		return System.currentTimeMillis() - lastActivation  > ABILITY_COOLDOWN_SECONDS * 1000L;
 	}
 	public UUID getPlayerUUID() {
 		return this.playerUUID;
@@ -30,16 +29,26 @@ public class PlayerInstance {
 		this.ability = abilityClass;
 	}
 
+	public void sendCooldownMessage(Player p){
+		p.sendMessage("Cooldown has not Finished\nRemaining: " + (ABILITY_COOLDOWN_SECONDS - ((System.currentTimeMillis() - lastActivation) / 1000)));
+	}
+
 	public void playerHitAbility(EntityDamageByEntityEvent event){
 		if(isPlayerDead) return;
-		if(!hasCooldownPassed())return;
+		if(!hasCooldownPassed()){
+			sendCooldownMessage((Player) event.getDamager());
+		return;
+		}
 		ability.playerHitAbility(event);
 		lastActivation = System.currentTimeMillis();
 	}
 
 	public void rightClickAbility(PlayerInteractEvent event){
 		if(isPlayerDead)return;
-		if(!hasCooldownPassed())return;
+		if(!hasCooldownPassed()){
+			sendCooldownMessage(event.getPlayer());
+			return;
+		}
 		ability.rightClickAbility(event);
 		lastActivation = System.currentTimeMillis();
 	}
--- src/main/java/me/luna/playerClasses/Gravity.java
+++ src/main/java/me/luna/playerClasses/Gravity.java
@@ -4,11 +4,13 @@ import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
 public class Gravity extends AbilityTemplate{
 
-    private static int PASSIVE_HEIGHT = 40;
-    private static int OFFENSE_HEIGHT = 30;
+    private static final int PASSIVE_HEIGHT = 20;
+    private static final int OFFENSE_HEIGHT = 15;
     private Location calculateTPHeight(Location baseLocation, int height){
         return baseLocation.add(0,height,0);
     }
@@ -22,5 +24,10 @@ public class Gravity extends AbilityTemplate{
     @Override
     public void rightClickAbility(PlayerInteractEvent event) {
         player.teleport(calculateTPHeight(player.getLocation(),PASSIVE_HEIGHT));
+        player.addPotionEffect(new PotionEffect(PotionEffectType.SLOW_FALLING,100,1));
+    }
+    @Override
+    public AbilityTemplate createNewInstance() {
+        return new Gravity();
     }
 }
