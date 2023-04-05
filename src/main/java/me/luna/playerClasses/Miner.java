0802ccdb0f1b4037e350bdc496657e10426550ae
Luna T <luna_trn@tutanota.com>
2023-04-05 16:26:13 +1000

    Remade Miner Formula
    Author: Luna
    
    Loops through x,y,z until all blocks are broken

--- src/main/java/me/luna/playerClasses/Miner.java
+++ src/main/java/me/luna/playerClasses/Miner.java
@@ -2,31 +2,36 @@ package me.luna.playerClasses;
 
 import org.bukkit.Location;
 import org.bukkit.Material;
-import org.bukkit.entity.Player;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
-import org.bukkit.inventory.ItemStack;
-
-import java.lang.reflect.Array;
-import java.util.LinkedList;
-import java.util.Random;
+import org.bukkit.util.Vector;
 
 public class Miner extends AbilityTemplate {
     private static int OFFENSE_RADIUS = 3;
     private static int PASSIVE_RADIUS = 5;
+    private static Vector ADD_X = new Vector(1,0,0);
+    private static Vector ADD_Y = new Vector(0,1,0);
+    private static Vector ADD_Z = new Vector(0,0,1);
 
     private void destroyBlock(Location blockLocation){
         if(blockLocation.getBlock().getType() == Material.AIR)return;
         blockLocation.getBlock().breakNaturally();
     }
     private void mineArea(Location loc, int radius){
-        Location end = loc.multiply(radius);
-        Location center = loc.add(loc.add(end).multiply(0.5));
-        for(int i = 0; i < radius;i++){
-            destroyBlock(center.multiply(i));
+        Location cursorLocation = loc;
+        for(int x = -radius; x  < radius; x++){
+            for(int y = -radius; y < radius; y++){
+                for(int z = -radius; z < radius; z++){
+                    cursorLocation.add(ADD_Z);
+                    destroyBlock(cursorLocation);
+                }
+                cursorLocation.add(ADD_Y);
+                destroyBlock(cursorLocation);
+            }
+            cursorLocation.add(ADD_X);
+            destroyBlock(cursorLocation);
         }
     }
-
     @Override
     public void playerHitAbility(EntityDamageByEntityEvent event) {
         super.playerHitAbility(event);
