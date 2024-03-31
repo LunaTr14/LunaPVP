67b9ae6cc5a938d18e1c419c3e3040eabafee2cc
Luna <null@null.null>
2024-03-31 14:25:37 +1100

    Functioning and Refactored Gravity Class
    Author: Luna
    Date: 31/03/2024

--- src/main/java/me/luna/ability/Gravity.java
+++ src/main/java/me/luna/ability/Gravity.java
@@ -7,22 +7,25 @@ import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 
-public class Gravity extends AbilityTemplate{
-    private float DAMAGE_BOOSTER = 3f;
-    public int delay = 5;
+public class Gravity extends AbilityTemplate {
+
+    public Gravity() {
+        delay = 5;
+    }
+
     @Override
-    public void activate(Event e) {
+    public boolean activate(Event e) {
         if (isEventPlayerInteract(e)) {
             PlayerInteractEvent interactEvent = (PlayerInteractEvent) e;
             Player p = ((PlayerInteractEvent) e).getPlayer();
-            if (isRightClick(interactEvent.getAction())) {
+            if (isRightClick(interactEvent.getAction()) || isLeftClick(interactEvent.getAction())) {
                 p.getPlayer().teleport(p.getLocation().add(0, 10, 0));
                 p.addPotionEffect(new PotionEffect(PotionEffectType.SLOW_FALLING, 20 * 15, 1));
             }
-        }
-        else if(isEventEntityHit(e)){
+        } else if (isEventEntityHit(e)) {
             EntityDamageByEntityEvent damageEvent = (EntityDamageByEntityEvent) e;
-            damageEvent.getDamager().teleport(damageEvent.getDamager().getLocation().add(0,7.5,0));
+            damageEvent.getEntity().teleport(damageEvent.getDamager().getLocation().add(0, 7.5, 0));
         }
+        return true;
     }
 }
